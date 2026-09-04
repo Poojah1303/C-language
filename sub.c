df3faf5ec2649051bcb8122db915583e2dcd3fed
Poojah1303 <poojahardasani168@gmail.com>
2026-09-04 09:37:01 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,sub;
+    printf("Enter the numbers:\n");
+    scanf("%d %d",&a,&b);
+    sub=a-b;
+    printf("The subtraction of two numbers: %d\n",sub);
+    
+
+}
