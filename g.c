dd7355a7b3945594e9338c77ba570a269c8ea987
Poojah1303 <poojahardasani168@gmail.com>
2026-09-04 23:09:09 +0530

    Add files via upload

---
+++ g.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float g,kg;
+    printf("Enter the weight in kilograms:\n");
+    scanf("%f",&kg);
+    g=kg*1000;
+    printf("The given weight in grams is:%2f\n",g);
+
+
+}
---
+++ kg.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float g,kg;
+    printf("Enter the weight in grams:\n");
+    scanf("%f",&g);
+    kg=g/1000;
+    printf("The given weight in kilograms is:%2f\n",kg);
+
+
+}
