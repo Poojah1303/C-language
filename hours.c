e44a850738ea0ea5bb6848a143a00bff86787c8c
Poojah1303 <poojahardasani168@gmail.com>
2026-09-04 15:11:02 +0530

    Add files via upload

---
+++ hours.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int hours,minutes;
+    printf("Enter the time in hours:\n");
+    scanf("%d",&hours);
+    minutes=60*hours;
+    printf("The given time in minutes is:%d\n",minutes);
+
+    
+
+    
+}
