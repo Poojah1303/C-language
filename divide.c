7aba89f2272a4dc3d3b2947f1b8a1a58b15cf054
Poojah1303 <poojahardasani168@gmail.com>
2026-09-04 10:54:07 +0530

    Add files via upload

---
+++ divide.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    float a,b,d;
+    printf("Enter the numbers:\n");
+    scanf("%f %f",&a,&b);
+    d=a/b;
+    printf("The division of two numbers is:%2f\n",d);
+}
